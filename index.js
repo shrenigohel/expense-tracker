e03512ec183341d0623c034b7c65477344631c96
shrenigohel <shrenigohel1420@gmail.com>
2025-08-25 17:38:07 +0530

    added storage

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log(" Welcome to Expense Tracker CLI!");
 console.log("Commands: add, list, delete, total");
+
+const { loadExpenses, saveExpenses } = require("./storage");
+const command = process.argv[2];
